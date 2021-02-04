6654e9c2db16b8198823deb628a818803c9e549a
kevin frias <kevinfrias@ymail.com>
2021-02-04 15:16:42 -0600

    Added nav

---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => {
+    return(
+        <StyledNav>
+            <h1><a id='logo' href="#">Capture</a></h1>
+            <ul>
+                <li>
+                    <a href="#">1. About Us</a>
+                </li>
+                <li>
+                    <a href="#">2. Our Work</a>
+                </li>
+                <li>
+                    <a href="#">3. Contact Us</a>
+                </li>
+            </ul>
+        </StyledNav>
+    );
+}
+
+const StyledNav = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 5rem;
+    background: #282828;
+    a {
+        font-size: 0.8rem;
+        color: white;
+        text-decoration: none;
+    }
+    ul {
+        display: flex;
+        list-style: none;
+    }
+    #logo {
+        font-size: 1.8rem;
+        font-family: "Lobster", cursive;
+        font-weight: lighter;
+    }
+    li {
+        padding-left: 4rem;
+        position: relative;
+    }
+`;
+
+export default Nav;
