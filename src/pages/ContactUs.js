d543f6d9ddbbeb55f1c8213f92a3df0b19e72ae4
kevin frias <kevinfrias@ymail.com>
2021-02-05 14:38:35 -0600

    Added styling&animation to contactus

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,75 @@
 import React from 'react';
 //Animations
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+import styled from 'styled-components';
 
 const ContactUs = () => {
     return(
-        <motion.div 
+        <ContactStyle
             exit="exit" 
             variants={pageAnimation} 
             initial="hidden" 
             animate="show"
             style={{ background:'#fff' }}
         >
-            <h1>Contact</h1>
-        </motion.div>
+            <Title>
+                <Hide>
+                    <motion.h2 variants={titleAnim}>Get In Touch.</motion.h2>
+                </Hide>
+            </Title>
+            <div>
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>Send Us A Message</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>Send An Email</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>Social Media</h2>
+                    </Social>
+                </Hide>
+            </div>
+        </ContactStyle>
     );
-}
+};
+
+const ContactStyle = styled(motion.div)`
+    padding: 5rem 5rem;
+    color: #353535;
+    min-height: 90vh;
+`;
+
+const Title = styled.div`
+    margin-bottom: 3rem;
+    color: black;
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #353535;
+`;
+
+const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+        margin: 1rem;
+    }
+`;
 
 export default ContactUs;
